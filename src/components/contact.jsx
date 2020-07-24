362776edf77f54c3706e5194890cc83aa9b67523
Fabio Carrella <fabiocarrella@gmail.com>
2020-07-24 17:23:47 +0200

    Edit contact form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'contact', data }),
+      body: encode({ 'form-name': 'contact', ...data }),
     })
       .then(() => alert('Success!'))
       .catch((error) => alert(error));
